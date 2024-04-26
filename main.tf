aee99c1a67d023c34302e27fd76d5a9054dba5e0
Ahmer Khan <Ahmerkhan@email.com>
2024-04-26 17:48:55 +0500

    added s3 as backend bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-123"
+    key    = "state"
+    region = "us-east-1"  
+    
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -48,7 +58,8 @@ resource "aws_instance" "example" {
   ami           = "ami-080e1f13689e07408"
   instance_type = var.instance_type
   subnet_id     = aws_subnet.main.id
-
+  security_groups = [aws_security_group.nginx_sg.name]
+  
   tags = {
     Name = "Example Instance"
   }
@@ -61,3 +72,54 @@ resource "aws_s3_bucket" "b" {
     Name = "My S3 Bucket"
   }
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket-123"  
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-security-group"
+  description = "Security group for Nginx web server"
+  vpc_id      = aws_vpc.main.id  # Assumes you have a VPC defined as aws_vpc.main
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-security-group"
+  }
+}
+
